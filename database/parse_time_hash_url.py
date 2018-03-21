a709a1edb0fe6757385ab050b7bf3f2b735e725b
agrgn42 <angela.schopke@gmail.com>
2018-03-21 02:58:30 -0400

    addressing Rep. Harris' lack of tweet_text issue

--- database/parse_time_hash_url.py
+++ database/parse_time_hash_url.py
@@ -286,7 +286,7 @@ class followyourleaders(object):
 
 	def initialize_database(self,num_tweets_shown):
 
-		self.update_timeline_collection(collection_tweet.find())
+		self.update_timeline_collection(collection_tweet)
 		# self.update_hashtag_collection(collection_tweet.find())
 		# self.update_url_collection(collection_tweet.find())
 		# self.update_leaders(num_tweets_shown)
