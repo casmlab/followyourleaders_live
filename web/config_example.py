0f9e040b604a241d0846d251bb73fc8492c71e1b
Libby Hemphill <libbyh@gmail.com>
2018-03-29 11:11:42 -0400

    Use environment variable to set config.py location

--- web/config_example.py
+++ web/config_example.py
@@ -1,17 +1,3 @@
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    # MONGO_URI = 'mongodb://localhost:27017/followyourleaders_dev'
-    # MONGO_DBNAME='followyourleaders_dev'
-
-class ProductionConfig(Config):
-    DEBUG = True
-    MONGO_URI = 'mongodb://localhost:27017/followyourleaders'
-    MONGO_DBNAME='followyourleaders'
-
-
-class DevelopmentConfig(Config):
-    DEBUG = True
-    MONGO_URI = 'mongodb://localhost:27017/followyourleaders_dev'
-    MONGO_DBNAME='followyourleaders_dev'
-
+DEBUG = True
+MONGO_URI = 'mongodb://localhost:27017/followyourleaders_dev'
+MONGO_DBNAME='followyourleaders_dev'
